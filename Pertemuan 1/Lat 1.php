c8ae6f7979877d6649548db7ae3513e9c7048bfd
SidikYoga <162028670+SidikYoga@users.noreply.github.com>
2024-03-03 20:14:34 +0700

    Add files via upload

---
+++ Pertemuan 1/Lat 1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    print"<pre>";
+    print"<center><font size= 10 color='red'>BIODATA</font></center>"; 
+    
+    print"<pre>";
+    print"<font size= 10 color='blue'>Nama      :<b> Sidik Yoga Pratama </b></font>"; 
+    
+    print"<pre>";
+    print"<font size= 10 color='blue'>Email     : yogasidik0@gmail.com</font>"; 
+    
+    print"<pre>";
+    print"<font size= 10 color='blue'>Jurusan   : Teknik Informatika </font>"; 
+    
+    print"<pre>";
+    print"<font size= 10 color='blue'>Hoby      : Futsal </font>";  
+    
+  ?>
+  
+  
+</body>
+</html>
