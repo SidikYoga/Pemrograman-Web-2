1cf5f7afb304d866c822042b914f3478fd50b1b9
SidikYoga <162028670+SidikYoga@users.noreply.github.com>
2024-05-08 20:01:23 +0700

    output.php

--- UTS_SidikYogaPratama/output.php
+++ UTS_SidikYogaPratama/output.php
@@ -2,22 +2,23 @@
 <html>
 <head>
     <title>Data Group B Piala Asia U-23</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
+     <title>Data Group B Piala Asia U-23</title>
+        <style>
+            table {
+                border-collapse: separate;
+                width: 100%;
+            }
 
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: left;
-        }
+            th, td {
+                border: 2px solid black;
+                padding: 8px;
+                text-align: left;
+            }
 
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
+            th {
+                background-color: #f2f2f2;
+            }
+        </style>
 </head>
 <body>
     <h1>Data Group B Piala Asia Qatar U-23</h1>
